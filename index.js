56ed9203154c41933494faa13e0f471995bfda7c
Sebastian Ljungkrantz <sebastian.ljungkran@learnet.se>
2021-10-07 09:58:16 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+class Display {
+
+    constructor() {
+
+
+    }
+
+    putPixel( x, y, color ) { }
+
+    line( x1, y1, x2, y2, color ) { }
+
+    circle( x, y, r, color ) { }
+
+    rectangle( x1, y1, x2, y2, color ) { }
+
+    clear( color ) { }
+
+    resize( x, y ) { }
+
+    textOut( x, y, color, string ) { }
+
+    scrollLeft() { }
+
+    scrollRight() { }
+
+    scrollUp() { }
+
+    scrollDown() { }
+
+    pscrollLeft() { }
+
+    pscrollRight() { }
+
+    pscrollUp() { }
+
+    pscrollDown() { }
+}
