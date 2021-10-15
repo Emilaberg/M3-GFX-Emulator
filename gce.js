fafb74ba6169720a5ac89c5b302bc96ef9db060b
Erik <erik.pehrsson@elev.ga.ntig.se>
2021-10-15 09:28:02 +0200

    Implemented code for the functions render, resize, line and rectangle

--- gce.js
+++ gce.js
@@ -1,120 +1,159 @@
-// Testing many snippets of code, 
-// adjustments necessary for the program to work would be implemented in a later version.
-
 const canvas = document.querySelector("#canvas");
+let context = canvas.getContext('2d');
 
-canvas.width = window.innerWidth-100;
-canvas.height = window.innerWidth-100;
-
-let plane = canvas.getcontext('2d');
-for (let i = 0; i < 6; i++) {
-    for (let j = 0; j < 6; j++) {
-        plane.fillStyle = 'rgb(' + Math.floor(255 - 42.5 * i) + ', ' +
-                        Math.floor(255 - 42.5 * j) + ', 0)';
-        plane.fillRect(j * 25, i * 25, 25, 25);
-    }
-}
+canvas.width = window.innerWidth - 100;
+canvas.height = window.innerHeight - 100;
 
 class Display {
 
     constructor() {
-        width (pixels)
-        height (pixels)
-        plane (width, height)
-        zoom ()
-        lock (bool)
+        this.width = 100
+        this.height = 100
+        this.plane = new Array(this.width * this.height)
+        this.zoom = 2
+        this.lock = false
     }
 
     putPixel(x, y, color) {
-        this.plane[y*this.width+x] = color;
-        return;
+        if (x >= this.width) {
+            x = this.width - 1
+        }
+        this.plane[y * this.width + x] = color;
     }
 
-    line(x1, y1, x2, y2, color) { 
-        plane.moveTo(x1, y1);
-        plane.lineTo(x2, y2);
-        plane.fillStyle = color;
-        plane.stroke();
-        return;
+    line(x1, y1, x2, y2, color) {
+        if (x1 > this.width || x2 > this.width || y1 > this.height || y2 > this.height) {
+            alert("line point is outside of display's plane");
+            return;
+        }
+        let diffX = x2 - x1;
+        let diffY = y2 - y1;
+
+        // The pixels we change should not be further than 1 pixel apart:
+        // (So distance is also number of pixels that we need to change)
+        // let distance = Math.sqrt(diffX * diffX + diffY * diffY);
+        let distance = Math.abs(diffX) > Math.abs(diffY) ? Math.abs(diffX) : Math.abs(diffY);
+
+        diffX /= distance;
+        diffY /= distance;
+        for (let i = 0; i <= distance; i++) {
+            this.putPixel(Math.round(x1 + diffX * i), Math.round(y1 + diffY * i), color);
+        }
     }
 
     circle(x, y, r, color) {
-        plane.beginPath();
-        plane.arc(x, y, r, 0, 2 * Math.PI);
-        plane.fillStyle = color;
-        plane.stroke();
-        return;
+        
     }
 
-    rectangle(x1, y1, x2, y2, color) { 
-        plane.fillRect(x1, y1, x2, y2);
-        plane.fillStyle = color;
-        return;
+    rectangle(x1, y1, x2, y2, color) {
+        if (x1 > this.width || x2 > this.width || y1 > this.height || y2 > this.height) {
+            alert("rectangle point is outside of display's plane");
+            return;
+        }
+
+        // top line - ─
+        this.line(x1, y1, x2, y1, color)
+
+        // right line - |
+        this.line(x2, y1, x2, y2, color)
+
+        // bottom line - ─
+        this.line(x1, y2, x2, y2, color)
+
+        // left line - |
+        this.line(x1, y1, x1, y2, color)
     }
 
-    clear(color) { 
-        if(color > 255 || color < 0) { 
+    clear(color) {
+        if (color > 255 || color < 0) {
             alert("color range error");
             return;
         }
 
-        for (let i = 0; i<this.width*this.height; i++) {
+        for (let i = 0; i < this.width * this.height; i++) {
             this.plane[i] = color;
-            return;
         }
     }
 
-    resize(x, y) { 
-    // Event handler to resize the canvas when the document view is changed
-    window.addEventListener('resize', resizeCanvas, false);
-
-    function resizeCanvas() {
-    plane.width = window.innerWidth;
-    plane.height = window.innerHeight;
-
-    // Redraw everything after resizing the window
-    draw(); 
-        }
-        return;
+    resize(x, y) {
+        this.width = x;
+        this.height = y;
+        this.plane = new Array(this.width * this.height)
     }
 
-    textOut( x, y, color, string ) { 
-        return;
+    textOut(x, y, color, string) {
+
     }
 
-    scrollLeft() { 
-        return;
+    scrollLeft() {
+
     }
 
-    scrollRight() { 
-        return;
+    scrollRight() {
+
     }
 
-    scrollUp() { 
-        return;
+    scrollUp() {
+
     }
 
-    scrollDown() { 
-        return;
+    scrollDown() {
+
     }
 
-    pscrollLeft() { 
-        return;
+    pscrollLeft() {
+
     }
 
-    pscrollRight() { 
-        return;
+    pscrollRight() {
+
     }
 
-    pscrollUp() { 
-        return;
+    pscrollUp() {
+
     }
 
-    pscrollDown() { 
-        return;
+    pscrollDown() {
+
     }
 }
 
-render(); {
-    return;
+let display = new Display();
+function render() {
+    let xZoom = Math.round(canvas.width / display.width);
+    let yZoom = Math.round(canvas.height / display.height);
+    let smallestZoom = xZoom < yZoom ? xZoom : yZoom;
+    display.zoom = smallestZoom;
+
+    let outputWidth = display.width * display.zoom;
+    let outputHeight = display.height * display.zoom;
+    let imageData = context.getImageData(0, 0, outputWidth, outputHeight)
+
+    for (let y = 0; y < outputHeight; y++) {
+        let planeWidth = display.width * Math.floor(y / display.zoom);
+        for (let x = 0; x < outputWidth; x++) {
+            let planeOffset = Math.floor(x / display.zoom) + planeWidth;
+            let color = display.plane[planeOffset] || 0;
+
+            let offset = x + y * outputWidth;
+            imageData.data[offset * 4] = color; // R
+            imageData.data[offset * 4 + 1] = color; // G
+            imageData.data[offset * 4 + 2] = color; // B
+            imageData.data[offset * 4 + 3] = 255; // A
+        }
+    }
+
+    context.putImageData(imageData, 0, 0)
+}
+setInterval(render, 1000 / 20);
+
+// Event handler to resize the canvas when the document view is changed
+window.addEventListener('resize', resizeCanvas, false);
+
+function resizeCanvas() {
+    display.resize(canvas.width, canvas.height)
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    // Redraw everything after resizing the window
+    render();
 }
