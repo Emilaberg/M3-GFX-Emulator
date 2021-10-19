24e895270cdab30145e21887d5ab9206bb1152b1
Erik <erik.pehrsson@elev.ga.ntig.se>
2021-10-19 10:29:00 +0200

    Implemented code for all scroll functions + updated the render function to clear the canvas when its resized + added random color function for testing consistency in other functions + implemented default resize, color and composed work of art with adapted positioning and size set via procentages of display's width and height

--- gce.js
+++ gce.js
@@ -20,6 +20,13 @@ class Display {
         }
         this.plane[y * this.width + x] = color;
     }
+    getPixel(x, y) {
+        if (x >= this.width) {
+            x = this.width - 1
+        }
+        return this.plane[y * this.width + x];
+    }
+
 
     line(x1, y1, x2, y2, color) {
         if (x1 > this.width || x2 > this.width || y1 > this.height || y2 > this.height) {
@@ -42,7 +49,7 @@ class Display {
     }
 
     circle(x, y, r, color) {
-        
+
     }
 
     rectangle(x1, y1, x2, y2, color) {
@@ -75,6 +82,14 @@ class Display {
         }
     }
 
+    random() {
+        for (let y = 0; y < this.height; y++) {
+            for (let x = 0; x < this.width; x++) {
+                this.putPixel(x, y, Math.round(Math.random() * 255));
+            }
+        }
+    }
+
     resize(x, y) {
         this.width = x;
         this.height = y;
@@ -86,45 +101,102 @@ class Display {
     }
 
     scrollLeft() {
-
+        for (let y = 0; y < this.height; y++) {
+            for (let x = 1; x < this.width; x++) {
+                // this.putPixel(x - 1, y, this.getPixel(x, y));
+                this.plane[y * this.width + x - 1] = this.plane[y * this.width + x];
+            }
+            this.putPixel(this.width - 1, y, 0);
+        }
     }
 
     scrollRight() {
-
+        for (let y = 0; y < this.height; y++) {
+            for (let x = this.width - 1; x > 0; x--) {
+                // this.putPixel(x, y, this.getPixel(x - 1, y));
+                this.plane[y * this.width + x] = this.plane[y * this.width + x - 1];
+            }
+            this.putPixel(0, y, 0);
+        }
     }
 
     scrollUp() {
-
+        for (let y = 1; y < this.height; y++) {
+            for (let x = 0; x < this.width; x++) {
+                this.putPixel(x, y - 1, this.getPixel(x, y));
+                // this.plane[(y - 1) * this.width + x] = this.plane[y * this.width + x];
+            }
+        }
+        this.line(0, this.height - 1, this.width - 1, this.height - 1, 0);
     }
 
     scrollDown() {
-
+        for (let y = this.height - 1; y > 0; y--) {
+            for (let x = 0; x < this.width; x++) {
+                this.putPixel(x, y, this.getPixel(x, y - 1));
+                // this.plane[y * this.width + x] = this.plane[(y - 1) * this.width + x];
+            }
+        }
+        this.line(0, 0, this.width - 1, 0, 0);
     }
 
     pscrollLeft() {
-
+        for (let y = 0; y < this.height; y++) {
+            let temp = this.getPixel(0, y);
+            for (let x = 1; x < this.width; x++) {
+                // this.putPixel(x - 1, y, this.getPixel(x, y));
+                this.plane[y * this.width + x - 1] = this.plane[y * this.width + x];
+            }
+            this.putPixel(this.width - 1, y, temp);
+        }
     }
 
     pscrollRight() {
-
+        for (let y = 0; y < this.height; y++) {
+            let temp = this.getPixel(this.width - 1, y);
+            for (let x = this.width - 1; x > 0; x--) {
+                // this.putPixel(x, y, this.getPixel(x - 1, y));
+                this.plane[y * this.width + x] = this.plane[y * this.width + x - 1];
+            }
+            this.putPixel(0, y, temp);
+        }
     }
 
     pscrollUp() {
-
+        for (let x = 0; x < this.width; x++) {
+            let temp = this.getPixel(x, 0);
+            for (let y = 1; y < this.height; y++) {
+                this.putPixel(x, y - 1, this.getPixel(x, y));
+                // this.plane[(y - 1) * this.width + x] = this.plane[y * this.width + x];
+            }
+            this.putPixel(x, this.height - 1, temp);
+        }
     }
 
     pscrollDown() {
-
+        for (let x = 0; x < this.width; x++) {
+            let temp = this.getPixel(x, this.height - 1);
+            for (let y = this.height - 1; y > 0; y--) {
+                this.putPixel(x, y, this.getPixel(x, y - 1));
+                // this.plane[y * this.width + x] = this.plane[(y - 1) * this.width + x];
+            }
+            this.putPixel(x, 0, temp);
+        }
     }
 }
 
 let display = new Display();
 function render() {
+    // Determine Zoom:
     let xZoom = Math.round(canvas.width / display.width);
     let yZoom = Math.round(canvas.height / display.height);
     let smallestZoom = xZoom < yZoom ? xZoom : yZoom;
     display.zoom = smallestZoom;
 
+    // Clear canvas (useful if display is resized):
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Update canvas from display:
     let outputWidth = display.width * display.zoom;
     let outputHeight = display.height * display.zoom;
     let imageData = context.getImageData(0, 0, outputWidth, outputHeight)
@@ -147,11 +219,18 @@ function render() {
 }
 setInterval(render, 1000 / 20);
 
+
+resizeCanvas();
+display.clear(255)
+writeCanvas(Math.floor(display.width * 0.1), Math.floor(display.height * 0.1), Math.floor(display.width * 0.8), Math.floor(display.height * 0.8));
+
+
 // Event handler to resize the canvas when the document view is changed
 window.addEventListener('resize', resizeCanvas, false);
 
 function resizeCanvas() {
     display.resize(canvas.width, canvas.height)
+    display.clear(255);
 
     context.clearRect(0, 0, canvas.width, canvas.height);
     // Redraw everything after resizing the window
